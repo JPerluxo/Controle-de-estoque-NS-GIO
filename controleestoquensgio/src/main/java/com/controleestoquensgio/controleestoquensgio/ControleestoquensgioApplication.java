19ed071c1a9b08a19072464137b726d77d1f487c
TiagoGomesTemoteo <tiago.gomes.temoteo@gmail.com>
2022-03-13 15:41:45 -0300

    Criação do Projeto

---
+++ controleestoquensgio/src/main/java/com/controleestoquensgio/controleestoquensgio/ControleestoquensgioApplication.java
@@ -0,0 +1,13 @@
+package com.controleestoquensgio.controleestoquensgio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleestoquensgioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleestoquensgioApplication.class, args);
+	}
+
+}
---
+++ controleestoquensgio/src/test/java/com/controleestoquensgio/controleestoquensgio/ControleestoquensgioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controleestoquensgio.controleestoquensgio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleestoquensgioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
